62ac060f1873f382f754c6c719cbddede2647dc4
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2017-12-22 15:50:19 +0100

    removed django_spaghetti

--- depictions/settings/base.py
+++ depictions/settings/base.py
@@ -27,8 +27,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crispy_forms',
-    'django_spaghetti',
-    'django_filters',
     'django_tables2',
     'rest_framework',
     'idprovider',
--- django_spaghetti/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from django_spaghetti.views import plate
-
-app_name = 'django_spaghetti'
-
-urlpatterns = [
-    url(r'^$', plate, name='plate'),
-]
