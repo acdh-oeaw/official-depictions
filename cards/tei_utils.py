1a6d12beb74d8701f8fa1594a39207b08dcc9a62
csae8092 <peter.andorfer@oeaw.ac.at>
2020-07-21 17:56:21 +0200

    TEI serialisation more or less done

--- cards/tei_utils.py
+++ cards/tei_utils.py
@@ -6,10 +6,15 @@ from django.conf import settings
 from django.template.loader import get_template
 
 
-def card_to_tei(card_object):
+def card_to_tei_node(card_object):
     template = get_template('cards/card_tei.xml')
     context = {'object': card_object}
     temp_str = f"{template.render(context=context)}"
     node = ET.fromstring(temp_str)
+    return node
+
+
+def card_to_tei(card_object):
+    node = card_to_tei_node(card_object)
     xml = ET.tostring(node, pretty_print=True, encoding='UTF-8')
     return xml
